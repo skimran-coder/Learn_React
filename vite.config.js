d5913318049b7d27a64c9f349e4f43fc2c463a12
Sk. Imran Hussain <skimranpc@gmail.com>
2025-01-05 15:13:39 +0530

    minor card style fix for mobile

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true, // Allows external devices to access
+    port: 5173, // Default port
+  },
   plugins: [react()],
-})
+});
